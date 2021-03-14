865dfc2dab5bd61c24b1a471fef2ff25447043f7
pkeisrebpys <pkeisrebpys@gmail.com>
2021-03-14 15:10:10 +0900

    モータ制御課題のテストを修正

--- motor-torque-simple/test.c
+++ motor-torque-simple/test.c
@@ -34,12 +34,18 @@ int main(int argc, char **argv) {
   int error = 2;
 
   for (i = 0; i < 100; i++) {
-    float v_dcm = dcm_v(v_in,0.0f);
-    analogin[1] = get_dig(2.5f);
+    float v_dcm;
+    if ((i / 20) % 2 == 0) {
+      v_dcm = dcm_v(v_in,0.5f);
+      analogin[1] = get_dig(0.5f);
+    } else {
+      v_dcm = dcm_v(v_in,-0.5f);
+      analogin[1] = get_dig(-0.5f);
+    }
     analogin[0] = get_dig((v_in - v_dcm) / R);
     if (abs(analogin[0] - analogin[1]) > error) {
       count++;
-      if (count > 10) {
+      if (count > 3) {
         printf("NG\n");
         return 1;
       }
